db931271eda292f2f13730a4ca518187e9772ae8
PortableDoughnut <gwen.thelin@gmail.com>
2024-11-25 10:12:11 -0700

    Added .gitignore

---
+++ Animal Crossing/VillagerTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  VillagerTableViewCell.swift
+//  Animal Crossing
+//
+//  Created by Gwen Thelin on 11/25/24.
+//
+
+import UIKit
+
+class VillagerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
